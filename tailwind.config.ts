cf101b781e67969752032a1c33fb32016a4e303d
Suraj Mahto <surajmahto255@gmail.com>
2024-06-18 16:06:37 +0530

    first commit

---
+++ tailwind.config.ts
@@ -0,0 +1,26 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      boxShadow: {
+        'bottom-only': '0 4px 2px 0 rgba(0, 0, 0, 0.1)',
+      },
+      fontFamily: {
+        oswald: ['Oswald', 'sans-serif'],
+      },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
